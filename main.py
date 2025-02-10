a797d3606ff99b28473b528fd6ceafab3cc35552
IndyMage <edwardhorner@gmail.com>
2025-02-10 14:22:54 -0800

    reworked main.py for L9

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     print(file_contents)
+    print(words(file_contents))
 
-def words():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        words = len(file_contents.split())
-    print(words)
+def words(text):
+    words = len(text.split())
+    return(words)
+
 
 main()
