4860a0f667fb34fe8bdef0e2bb6b38e035fa3aae
IndyMage <edwardhorner@gmail.com>
2025-02-11 21:35:25 -0800

    working but too many elif

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+   #print(file_contents)
     print(words(file_contents))
     print(chars(file_contents))
 
@@ -37,15 +37,145 @@ def chars(text):
         "w" : 0,
         "x" : 0,
         "y" : 0,
-        "z" : 0
+        "z" : 0,
+        " " : 0,
+        "." : 0,
+        "," : 0,
+        "!" : 0,
+        "?" : 0,
+        "*" : 0,
+        "1" : 0,
+        "2" : 0,
+        "3" : 0,
+        "4" : 0,
+        "5" : 0,
+        "6" : 0,
+        "7" : 0,
+        "8" : 0,
+        "9" : 0,
+        "0" : 0,
+        "(" : 0,
+        ")" : 0,
+        "-" : 0,
+        "@" : 0,
+        ":" : 0,
+        "/" : 0,
+        "$" : 0,
+        "'" : 0,
+        '"' : 0,
+        '%' : 0,
+        ";" : 0
+        
+       
     }
     lowered_string = text.lower()
     for char in lowered_string:
-        if char == letters[char]:
-            n = letters[char:n+1]
-            letters[char:n]
-        else:
-            print("not in the list")
+        if char == 'a':
+            letters['a'] += 1
+        elif char == 'b':
+            letters['b'] += 1
+        elif char == 'c':
+            letters['c'] += 1        
+        elif char == 'd':
+            letters['d'] += 1        
+        elif char == 'e':
+            letters['e'] += 1        
+        elif char == 'f':
+            letters['f'] += 1        
+        elif char == 'g':
+            letters['g'] += 1        
+        elif char == 'h':
+            letters['h'] += 1       
+        elif char == 'i':
+            letters['i'] += 1       
+        elif char == 'j':
+            letters['j'] += 1        
+        elif char == 'k':
+            letters['k'] += 1        
+        elif char == 'l':
+            letters['l'] += 1        
+        elif char == 'm':
+            letters['m'] += 1        
+        elif char == 'n':
+            letters['n'] += 1        
+        elif char == 'o':
+            letters['o'] += 1        
+        elif char == 'p':
+            letters['p'] += 1        
+        elif char == 'q':
+            letters['q'] += 1       
+        elif char == 'r':
+            letters['r'] += 1
+        elif char == 's':
+            letters['s'] += 1
+        elif char == 't':
+            letters['t'] += 1
+        elif char == 'u':
+            letters['u'] += 1
+        elif char == 'v':
+            letters['v'] += 1
+        elif char == 'w':
+            letters['w'] += 1
+        elif char == 'x':
+            letters['x'] += 1
+        elif char == 'y':
+            letters['y'] += 1
+        elif char == 'z':
+            letters['z'] += 1
+        elif char == " ":
+            letters[" "] += 1
+        elif char == ".":
+            letters["."] += 1
+        elif char == ",":
+            letters[","] += 1
+        elif char == "!":
+            letters["!"] += 1
+        elif char == "?":
+            letters["?"] += 1
+        elif char == "1":
+            letters["1"] += 1
+        elif char == "2":
+            letters["2"] += 1
+        elif char == "3":
+            letters["3"] += 1
+        elif char == "4":
+            letters["4"] += 1
+        elif char == "5":
+            letters["5"] += 1
+        elif char == "6":
+            letters["6"] += 1
+        elif char == "7":
+            letters["7"] += 1
+        elif char == "8":
+            letters["8"] += 1
+        elif char == "9":
+            letters["9"] += 1
+        elif char == "0":
+            letters["0"] += 1
+        elif char == "(":
+            letters["("] += 1
+        elif char == ")":
+            letters[")"] += 1
+        elif char == "@":
+            letters["@"] += 1
+        elif char == "$":
+            letters["$"] += 1
+        elif char == "-":
+            letters["-"] += 1
+        elif char == "/":
+            letters["/"] += 1
+        elif char == ":":
+            letters[":"] += 1
+        elif char == "'":
+            letters["'"] += 1
+        elif char == '"':
+            letters['"'] += 1
+        elif char == "*":
+            letters["*"] += 1
+        elif char == ";":
+            letters[";"] += 1
+        elif char == "%":
+            letters["%"] += 1       
     return(letters)
 
 main()
